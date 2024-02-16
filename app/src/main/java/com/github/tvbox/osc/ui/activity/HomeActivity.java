2a5df203d92964b17edf74e59be2db2c46c4dd2d
hello-jgq <137876700+hello-jgq@users.noreply.github.com>
2024-02-16 21:03:30 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -283,7 +283,7 @@ public class HomeActivity extends BaseActivity {
                             @Override
                             public void run() {
                                 if (!useCacheConfig)
-                                    Toast.makeText(HomeActivity.this, "自定义视频源成功", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(HomeActivity.this, "JGQTV欢迎您", Toast.LENGTH_SHORT).show();
                                 initData();
                             }
                         }, 50);
